543af2b3b9dd7d1d806e5c9907c6c18c070bfded
Hubert Wdowiak <hubert.wdowiak98@onet.pl>
2021-12-30 13:16:08 +0100

    Better commit

--- app/utils/app_base.py
+++ app/utils/app_base.py
@@ -4,7 +4,7 @@ from app.controllers import auth_controller, info_controller
 
 
 def create_app():
-    app = Flask(__name__, template_folder='templates', static_folder='static')
+    app = Flask(__name__, template_folder='../templates', static_folder='../static')
     app.app_context().push()
     app.secret_key = "super secret key"
 
