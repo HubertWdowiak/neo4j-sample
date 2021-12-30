5f479714ee662cdc6d4de2917221e4beaa8a7d8c
HubertWdowiak <hubert.wdowiak98@onet.pl>
2021-12-30 14:13:37 +0100

    better commit

--- app/utils/app_base.py
+++ app/utils/app_base.py
@@ -1,12 +1,12 @@
 from flask import Flask, request, render_template, redirect, url_for
 from app.utils import db
 from app.controllers import auth_controller, info_controller
-
+import os
 
 def create_app():
     app = Flask(__name__, template_folder='../templates', static_folder='../static')
     app.app_context().push()
-    app.secret_key = "super secret key"
+    app.secret_key = os.environ["secret"]
 
     app.register_blueprint(info_controller.bp)
     app.register_blueprint(auth_controller.auth)
